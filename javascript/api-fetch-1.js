a22927e092eebfb613447e024d42953c5c997a52
Nicolas <72019501+Serafear@users.noreply.github.com>
2021-02-10 15:58:02 +0100

    all apis are fetched
    now i need to adjust the buttons and the images to make it look good. I need to find how to push the name and price to the bottom of the pictures

--- javascript/api-fetch-1.js
+++ javascript/api-fetch-1.js
@@ -1,11 +1,12 @@
-
-    let url ='http://localhost:3000/api/teddies';
+//cette partie concerne le fetch des API
+let apiCallTeddies = function (teddies){
+  let url ='http://localhost:3000/api/teddies';
 
 
     fetch(url)
-    .then(function(response){
-        return response.json();}) //on peut avoir à la place du json du .arrayBuffer(), du .blol(), du .text(), et du .formData()
-    .then(function(data){
+      .then(function(response){
+        return response.json()}) //on peut avoir à la place du json du .arrayBuffer(), du .blol(), du .text(), et du .formData()
+      .then(function(data){
         console.log(data);
         document.querySelector('#name-t1').innerHTML = 'Nom : ' + data[0].name;
         document.querySelector('#price-t1').innerHTML = 'Prix : '+ data[0].price + ' eur';
@@ -34,8 +35,158 @@
         document.querySelector('#price-t5').innerHTML = 'Prix : '+ data[4].price + ' eur';
         document.querySelector('#imageUrl-t5').innerHTML = '<img src="http://localhost:3000/images/teddy_5.jpg">';
         document.querySelector('#description-t5').innerHTML = data[4].description;
-    });
+
+      
+      })
+      .catch(function(err) {console.log('Erreur : ' + err)});
+      
+}
+//api call caméras
+let apiCallCameras = function (Camera){
+  let url ='http://localhost:3000/api/cameras';
 
 
+    fetch(url)
+      .then(function(response){
+        return response.json()}) //on peut avoir à la place du json du .arrayBuffer(), du .blol(), du .text(), et du .formData()
+      .then(function(data){
+        console.log(data);
+        document.querySelector('#name-c1').innerHTML = 'Nom : ' + data[0].name;
+        document.querySelector('#price-c1').innerHTML = 'Prix : '+ data[0].price + ' eur';
+        document.querySelector('#imageUrl-c1').innerHTML = '<img src="http://localhost:3000/images/vcam_1.jpg">';
+        document.querySelector('#description-c1').innerHTML = data[0].description;
 
+        document.querySelector('#name-c2').innerHTML = 'Nom : ' + data[1].name;
+        document.querySelector('#price-c2').innerHTML = 'Prix : '+ data[1].price + ' eur';
+        document.querySelector('#imageUrl-c2').innerHTML = '<img src="http://localhost:3000/images/vcam_2.jpg">';
+        document.querySelector('#description-c2').innerHTML = data[1].description;
+
+        
+        document.querySelector('#name-c3').innerHTML = 'Nom : ' + data[2].name;
+        document.querySelector('#price-c3').innerHTML = 'Prix : '+ data[2].price + ' eur';
+        document.querySelector('#imageUrl-c3').innerHTML = '<img src="http://localhost:3000/images/vcam_3.jpg">';
+        document.querySelector('#description-c3').innerHTML = data[2].description;
+
+        
+        document.querySelector('#name-c4').innerHTML = 'Nom : ' + data[3].name;
+        document.querySelector('#price-c4').innerHTML = 'Prix : '+ data[3].price + ' eur';
+        document.querySelector('#imageUrl-c4').innerHTML = '<img src="http://localhost:3000/images/vcam_4.jpg">';
+        document.querySelector('#description-c4').innerHTML = data[3].description;
+
+        
+        document.querySelector('#name-c5').innerHTML = 'Nom : ' + data[4].name;
+        document.querySelector('#price-c5').innerHTML = 'Prix : '+ data[4].price + ' eur';
+        document.querySelector('#imageUrl-c5').innerHTML = '<img src="http://localhost:3000/images/vcam_5.jpg">';
+        document.querySelector('#description-c5').innerHTML = data[4].description;
+
+      
+      })
+      .catch(function(err) {console.log('Erreur : ' + err)});
+      
+}
+     
+//api call meubles
+let apiCallFurnitures = function (furniture){
+  let url ='http://localhost:3000/api/furniture';
+
+
+    fetch(url)
+      .then(function(response){
+        return response.json()}) //on peut avoir à la place du json du .arrayBuffer(), du .blol(), du .text(), et du .formData()
+      .then(function(data){
+        console.log(data);
+        document.querySelector('#name-f1').innerHTML = 'Nom : ' + data[0].name;
+        document.querySelector('#price-f1').innerHTML = 'Prix : '+ data[0].price + ' eur';
+        document.querySelector('#imageUrl-f1').innerHTML = '<img src="http://localhost:3000/images/oak_1.jpg">';
+        document.querySelector('#description-f1').innerHTML = data[0].description;
+
+        document.querySelector('#name-f2').innerHTML = 'Nom : ' + data[1].name;
+        document.querySelector('#price-f2').innerHTML = 'Prix : '+ data[1].price + ' eur';
+        document.querySelector('#imageUrl-f2').innerHTML = '<img src="http://localhost:3000/images/oak_2.jpg">';
+        document.querySelector('#description-f2').innerHTML = data[1].description;
+
+        
+        document.querySelector('#name-f3').innerHTML = 'Nom : ' + data[2].name;
+        document.querySelector('#price-f3').innerHTML = 'Prix : '+ data[2].price + ' eur';
+        document.querySelector('#imageUrl-f3').innerHTML = '<img src="http://localhost:3000/images/oak_3.jpg">';
+        document.querySelector('#description-f3').innerHTML = data[2].description;
+
+        
+        document.querySelector('#name-f4').innerHTML = 'Nom : ' + data[3].name;
+        document.querySelector('#price-f4').innerHTML = 'Prix : '+ data[3].price + ' eur';
+        document.querySelector('#imageUrl-f4').innerHTML = '<img src="http://localhost:3000/images/oak_4.jpg">';
+        document.querySelector('#description-f4').innerHTML = data[3].description;
+
+        
+        document.querySelector('#name-f5').innerHTML = 'Nom : ' + data[4].name;
+        document.querySelector('#price-f5').innerHTML = 'Prix : '+ data[4].price + ' eur';
+        document.querySelector('#imageUrl-f5').innerHTML = '<img src="http://localhost:3000/images/oak_5.jpg">';
+        document.querySelector('#description-f5').innerHTML = data[4].description;
+
+      
+      })
+      .catch(function(err) {console.log('Erreur : ' + err)});
+      
+}
+      
+apiCallTeddies();
+apiCallCameras();
+apiCallFurnitures();
+   
+
+//cette partie est destinée au slide
+    var slideIndex = 1;
+    showDivs(slideIndex);
+    
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+    
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("slider");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      x[slideIndex-1].style.display = "block";  
+    }
+//slide 2
+    var slideIndex = 1;
+    showDivs2(slideIndex);
+    
+    function plusDivs2(n) {
+      showDivs2(slideIndex += n);
+    }
+    
+    function showDivs2(n) {
+      var i;
+      var x = document.getElementsByClassName("slider2");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      x[slideIndex-1].style.display = "block";  
+    }
+
+    //slide 3
+    var slideIndex = 1;
+    showDivs3(slideIndex);
+    
+    function plusDivs3(n) {
+      showDivs3(slideIndex += n);
+    }
+    
+    function showDivs3(n) {
+      var i;
+      var x = document.getElementsByClassName("slider3");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      x[slideIndex-1].style.display = "block";  
+    }
 
